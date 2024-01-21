45c3e1ccd5dcdf0c7a0b7ab60f59923da438c065
yotayamamoto <ciel.222@icloud.com>
2024-01-21 14:37:43 +0900

    gallery実装完了

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ $('.humburger').click(function(){
     $(this).toggleClass('open'); 
     $(".sp_nav").toggleClass('open'); 
 })
+
+$(document).ready(function(){
+    $('.gallery_slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        arrows:false,
+        autoplay:true,
+        fade:true
+    });
+})
